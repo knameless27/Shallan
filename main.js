51fcb0877decfc874352dcd1fa41d1629c22a20c
unknown <knameless27@gmail.com>
2023-03-10 14:05:56 -0500

    feat

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ const express = require("express");
 const morgan = require("morgan");
 const api = require("./api");
 const app = express();
+// planet scale
 const mysql = require('mysql2')
 const connection = mysql.createConnection(process.env.DATABASE_URL)
 connection.end()
 //settings
 app.set("name", "Shallan");
-app.set("port", 3650);
+app.set("port", process.env.PORT);
 //middlewares
 app.use(morgan("dev"));
 app.use(express.json());
